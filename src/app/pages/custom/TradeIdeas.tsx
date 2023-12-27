28cacf12104f584b94edf79dc5adf80e42218eea
David Enikuomehin <enikuauth@gmail.com>
2023-12-27 22:31:17 +0100

    feat: create trade ideas page

--- src/app/pages/custom/TradeIdeas.tsx
+++ src/app/pages/custom/TradeIdeas.tsx
@@ -1,5 +1,36 @@
+import { useQuery } from 'react-query'
+import { fetchUserTradeSummary } from '../../../services/api'
+import { TradeWidget } from '../../../_metronic/partials/widgets/custom/TradeWidget'
+
 const TradeIdeas = () => {
-  return <div>TradeIdeas</div>
+  const {
+    data: trades,
+    isLoading,
+    isError,
+  } = useQuery('trades', fetchUserTradeSummary)
+
+  return (
+    <div>
+      <div className='row gy-5 g-xl-8 mb-5 mb-xl-10'>
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          trades?.data.summary_data.map((trade: any) => (
+            <div>
+              <TradeWidget
+                key={trade?._id}
+                data={trade}
+                className='card-xl-stretch mb-xl-8 justify-content-between gap-xl-5 flex-xl-row align-items-xl-center'
+              />
+            </div>
+          ))
+        )}
+
+        {isError ? <div>Error Loading Trades</div> : null}
+      </div>
+      {/* custom end::Row */}
+    </div>
+  )
 }
 
 export default TradeIdeas
