7021ead769c6074fc0d4f98995ea20106afb48a2
David Enikuomehin <enikuauth@gmail.com>
2023-12-31 12:17:31 +0100

    feat: create custom profile comp

--- src/app/pages/custom/profile/ProfileCustom.tsx
+++ src/app/pages/custom/profile/ProfileCustom.tsx
@@ -1,48 +1,116 @@
 import { useQuery } from 'react-query'
-import { fetchStatistics, fetchWallPosts } from '../../../../services/api'
-import { ChartsWidget1 } from '../../../../_metronic/partials/widgets/charts/ChartsWidget1'
-import { ListsWidget5 } from '../../../../_metronic/partials/widgets/lists/ListsWidget5'
-import { FeedsWidgetCustom } from '../../../../_metronic/partials/widgets/custom/FeedWidgetCustom'
+import {
+  fetchAllUsers,
+  fetchAssetsTradedStats,
+  fetchStatistics,
+  fetchUserTradeSummary,
+} from '../../../../services/api'
 import { TablesWidgetCustom } from '../../../../_metronic/partials/widgets/custom/TableWidgetCustom'
+import { ChartsWidgetCustom } from '../../../../_metronic/partials/widgets/charts/ChartsWidgetCustom'
+import { TradeWidget } from '../../../../_metronic/partials/widgets/custom/TradeWidget'
 
-const ProfileCustom = () => {
-  const { data: statistics, isLoading } = useQuery(
-    'statistics',
-    fetchStatistics
-  )
+const ProfileCustom: React.FC<{ userId: string }> = ({ userId }) => {
+  const {
+    data: statistics,
+    isLoading,
+    isError,
+  } = useQuery('statistics', () => fetchStatistics(userId))
 
-  const { data: posts, isLoading: isLoadingPosts } = useQuery(
-    'posts',
-    fetchWallPosts
-  )
+  const {
+    data: trades,
+    isLoading: isTradeLoading,
+    isError: isTradeError,
+  } = useQuery('trades', () => fetchUserTradeSummary(userId))
+
+  const { data: users } = useQuery('users', fetchAllUsers)
+
+  const {
+    data: assetsTradedStats,
+    isLoading: isAssetTradedLoading,
+    isError: isAssetTradedError,
+  } = useQuery('assetsTradedStats', () => fetchAssetsTradedStats(userId))
+
+  const assetsTradedStatsData = assetsTradedStats?.data.assetTrades
+
+  const currentUser = users?.data.find((user: any) => user._id === userId)
+
   return (
     <>
+      <div className='card card-flush mb-8'>
+        <div className='card-header'>
+          <h1 className='card-title fw-bold fs-4'>Profile Description</h1>
+        </div>
+
+        <div className='card-body pt-0 fs-5 text-muted mw-900px'>
+          {currentUser ? currentUser.profileDescription : 'NIl'}
+        </div>
+      </div>
+
+      {/* Assets Traded */}
+      {isAssetTradedLoading ? (
+        <div>Loading...</div>
+      ) : isAssetTradedError ? (
+        <div>Error fetching additional statistics</div>
+      ) : (
+        <ChartsWidgetCustom
+          className='mb-8'
+          title='Trade Direction'
+          seriesData={[
+            {
+              name: 'Crypto',
+              data: [assetsTradedStatsData.cryptoValue.toFixed(2)],
+            },
+            {
+              name: 'Forex',
+              data: [assetsTradedStatsData.forexValue.toFixed(2)],
+            },
+            {
+              name: 'Option',
+              data: [assetsTradedStatsData.optionValue.toFixed(2)],
+            },
+            {
+              name: 'Stock',
+              data: [assetsTradedStatsData.stockValue.toFixed(2)],
+            },
+          ]}
+        />
+      )}
+
       {isLoading ? (
         <div>Loading...</div>
+      ) : isError ? (
+        <div className='text-center fs-2 py-5'>Error fetching table data</div>
       ) : (
         <TablesWidgetCustom className='mb-8' data={statistics.data} />
       )}
-      <div className='row g-5 g-xxl-8'>
-        <div className='col-xl-6'>
-          {isLoadingPosts ? (
-            <div>Loading...</div>
-          ) : (
-            posts.data.results
-              .slice(0, 3)
-              .map((post: any) => (
-                <FeedsWidgetCustom
-                  key={post._id}
-                  className='mb-5 mb-xxl-8'
-                  data={post}
-                />
-              ))
-          )}
+
+      <div className='card card-flush'>
+        <div className='card-header'>
+          <div className='card-title fs-3'>Recent Trade Ideas</div>
         </div>
 
-        <div className='col-xl-6'>
-          <ChartsWidget1 className='mb-5 mb-xxl-8' />
-
-          <ListsWidget5 className='mb-5 mb-xxl-8' />
+        <div className='card-body pt-0 row'>
+          {isTradeLoading ? (
+            <div>Loading...</div>
+          ) : trades.data.summary_data.length > 0 ? (
+            trades.data.summary_data
+              .slice(0, 2)
+              .map((trade: any, index: number) => (
+                <div className='col-6 col-xl-12'>
+                  <TradeWidget
+                    key={trade._id ? trade._id : index}
+                    data={trade}
+                    className='card-xl-stretch mb-xl-8 justify-content-between gap-xl-5 flex-xl-row align-items-xl-center'
+                  />
+                </div>
+              ))
+          ) : isTradeError ? (
+            <div>Error Loading Trades</div>
+          ) : (
+            <div className='text-center fs-2 py-5'>
+              This user had not created any trades.
+            </div>
+          )}
         </div>
       </div>
     </>
