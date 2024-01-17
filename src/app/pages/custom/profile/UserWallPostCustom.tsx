8d055f6506d41fcc4a8c983674cc572acc181ddd
David Enikuomehin <enikuauth@gmail.com>
2024-01-17 13:19:59 +0100

    feat: implement infinite scrolling

--- src/app/pages/custom/profile/UserWallPostCustom.tsx
+++ src/app/pages/custom/profile/UserWallPostCustom.tsx
@@ -3,7 +3,7 @@ import { fetchWallPosts } from '../../../../services/api'
 import { FeedsWidgetCustom } from '../../../../_metronic/partials/widgets/custom/FeedWidgetCustom'
 import { Loading } from '../../../components/Loading'
 import NoData from '../../../components/NoData'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const UserWallPostCustom: React.FC<{ userId: string }> = ({ userId }) => {
   const [page, setPage] = useState(1)
@@ -11,31 +11,76 @@ const UserWallPostCustom: React.FC<{ userId: string }> = ({ userId }) => {
     data: posts,
     isLoading,
     isError,
-  } = useQuery('posts', () => fetchWallPosts(page))
+  } = useQuery(['posts', page], () => fetchWallPosts(page))
 
   const filteredPosts = posts?.data?.results?.filter(
     (item: any) => item?.author._id === userId
   )
+
+  const [loadingMore, setLoadingMore] = useState(false)
+
+  const [filteredPostsData, setFilteredPostsData] = useState<any>({
+    data: { results: [] },
+  })
+
+  useEffect(() => {
+    if (filteredPosts) {
+      setFilteredPostsData((prevData: any) => ({
+        data: {
+          results: [...prevData.data.results, ...filteredPosts.data.results],
+        },
+      }))
+      setLoadingMore(false)
+    }
+  }, [filteredPosts])
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        !loadingMore &&
+        window.innerHeight + document.documentElement.scrollTop >=
+          document.documentElement.offsetHeight - 200
+      ) {
+        // Reached the bottom of the page, load more posts
+        if (
+          page < parseInt((filteredPosts?.data?.totalRecords / 10).toFixed(0))
+        ) {
+          setPage((prevPage) => prevPage + 1)
+          setLoadingMore(true)
+        }
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
     <div className='row g-5 g-xxl-8 gap-1 justify-content-center'>
-      {isLoading ? (
-        <Loading />
-      ) : filteredPosts?.length > 0 ? (
-        filteredPosts?.map((post: any) => (
-          <FeedsWidgetCustom
-            key={post._id}
-            className='mb-8 col-lg-6'
-            data={post}
+      {
+        // isLoading ? (
+        //   <Loading />
+        // ) :
+        filteredPostsData?.length > 0 ? (
+          filteredPostsData?.map((post: any) => (
+            <FeedsWidgetCustom
+              key={post._id}
+              className='mb-8 col-lg-6'
+              data={post}
+            />
+          ))
+        ) : isError ? (
+          <NoData type='error' message="Error fetching user's wallpost" />
+        ) : (
+          <NoData
+            type='info'
+            message='This user has not created any wall posts.'
           />
-        ))
-      ) : isError ? (
-        <NoData type='error' message="Error fetching user's wallpost" />
-      ) : (
-        <NoData
-          type='info'
-          message='This user has not created any wall posts.'
-        />
-      )}
+        )
+      }
     </div>
   )
 }
